0efc1cda053ebc8a66ef5a996b122c8753da1a67
Prashasth Nair <prashasthnair1@gmail.com>
2023-02-28 20:06:18 +0530

    Contact form is working,Improved UI

--- html/js/contact.js
+++ html/js/contact.js
@@ -1,4 +1,5 @@
 const textarea = document.querySelector("textarea");
+
 textarea.addEventListener("keyup", (e) => {
   textarea.style.height = "63px";
   let scHeight = e.target.scrollHeight;
@@ -11,3 +12,20 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
   e.target.elements.email.value = '';
   e.target.elements.message.value = '';
 });
+
+function sendEmail() {
+  var name_ = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var mail_body = "Name: " + name_ + "<br>" + "Email: " + email + "<br>" + "Message: " + message;
+  Email.send({
+    // Host : "smtp.elasticemail.com",
+    SecureToken: "1f3ac6be-d073-4608-aea2-e4349b1af823",
+    To: 'prashasthnair@gmail.com',
+    From: "prashasthdm@gmail.com",
+    Subject: "New Contact Form Your Portfolio Website.",
+    Body: mail_body
+  }).then(
+    message => alert(message)
+  );
+}
