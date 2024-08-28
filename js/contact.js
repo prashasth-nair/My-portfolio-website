29817bd395562bc1ae64ba22d3101fbeae0aa7df
prashasth-nair <prashasthnair@gmail.com>
2024-08-28 10:59:10 +0530

    Updated resume

---
+++ js/contact.js
@@ -0,0 +1,65 @@
+const textarea = document.querySelector("textarea");
+const email_success_dialog = document.getElementById("dialog-box");
+const email_success_ok = document.getElementById("ok-button");
+const email = document.getElementById("email");
+
+email.addEventListener("input", function () {
+  const email = this.value.trim();
+  const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  this.classList.toggle("valid", isValid);
+});
+
+function showDialog() {
+  email_success_dialog.showModal();
+}
+email_success_ok.addEventListener("click", () => {
+  email_success_dialog.close();
+});
+
+textarea.addEventListener("keyup", (e) => {
+  textarea.style.height = "63px";
+  let scHeight = e.target.scrollHeight;
+  textarea.style.height = `${scHeight}px`;
+});
+
+document.getElementById("close_button").addEventListener("click", () => {
+  email_success_dialog.close();
+});
+
+document.querySelector("#contact-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = "";
+  e.target.elements.email.value = "";
+  e.target.elements.message.value = "";
+});
+
+function sendEmail() {
+  var name_ = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  const email_content = email.trim();
+  const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email_content);
+
+  if (!isValid) {
+    event.preventDefault(); // Prevent form submission
+    alert("Please enter a valid email address.");
+  } else {
+    var message = document.getElementById("message").value;
+    var mail_body =
+      "Name: " +
+      name_ +
+      "<br>" +
+      "Email: " +
+      email +
+      "<br>" +
+      "Message: " +
+      message;
+    Email.send({
+      // Host : "smtp.elasticemail.com",
+      SecureToken: "1f3ac6be-d073-4608-aea2-e4349b1af823",
+      To: "prashasthnair@gmail.com",
+      From: "prashasthdm@gmail.com",
+      Subject: "New Contact Form Your Portfolio Website.",
+      Body: mail_body,
+    }).then((message) => showDialog());
+  }
+}
---
+++ js/project.js
@@ -0,0 +1,31 @@
+const elements = document.querySelectorAll('.project_img');
+const project_details_elements = document.querySelectorAll('.project_details');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.style.animationPlayState = 'running';
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+elements.forEach((element) => {
+  observer.observe(element);
+});
+project_details_elements.forEach((project_details_element) => {
+  observer.observe(project_details_element);
+});
+
+
+const blob = document.getElementById("blob");
+
+blob.style.left = '80%';
+blob.style.top = '10%';
+document.body.onpointermove = event => {
+
+  const {clientX, clientY} = event;
+
+  blob.style.left = clientX+'px';
+  blob.style.top = clientY+'px';
+}
